f8e52570ca5329bde5c09fe165665bd44acf9b63
x123y123 <a0921444212@gmail.com>
2022-11-27 22:24:21 +0800

    update .gitignore

--- drone_with_cv/cmake/lib.cmake
+++
@@ -1,9 +0,0 @@
-# Project Library Configuration
-
-# Include subdirectories
-include_directories(${LIB_PATHS})
-
-# Add subdirectiories
-foreach(LIB_PATH ${LIB_PATHS})
-    add_subdirectory(${LIB_PATH})
-endforeach()
