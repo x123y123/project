19dcc3c8de190300ebd7e713327fc44c4ac76edd
tony <a0921444212@gmail.com>
2023-02-27 11:55:58 +0800

    still need to add IPC data_get thread

--- RL_DVFS/qlearning_gov.c
+++ RL_DVFS/qlearning_gov.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-#define NUM_STATES      3
+#define NUM_STATES      5
 #define NUM_ACTIONS     25
 #define NUM_FREQS       25
 #define ALPHA           0.1
@@ -10,14 +11,20 @@
 #define MAX_EPISODE     1000
 #define MAX_CPUFREQ     1907200
 #define MIN_CPUFREQ     115200
+#define MAX_CPU_VOLTS   5.5 //need to check!!!
+
 #define TEMP_LIMIT      46
+#define IPC_MAX         4
+#define RHO             5
 
 #define max(a, b) ((a > b) ? a : b)
 
 // Q-table
 double Q[NUM_STATES][NUM_ACTIONS];
+
 // cpufreq-table
 int cpufreq[NUM_FREQS] = {115200, 192000, 268800, 345600, 422400, 499200, 576000, 652800, 729600, 806400, 883200, 960000, 1036800, 1113600, 1190400, 1267200, 1344000, 1420800, 1497600, 1574400, 1651200, 1728000, 1804800, 1881600, 1907200};
+//double IPS_MAX = IPC_MAX * (cpufreq[NUM_FREQS - 1] * 1000.0);
 
 /*
 /sys/devices/virtual/thermal/thermal_zone<X>/temp
@@ -47,7 +54,7 @@ float get_cpu_power(void)
     return power_value;
 }
 
-float get_cpu_vol(void)
+float get_cpu_volts(void)
 {
     int vol_mV;
     fscanf(cpu_vol, "&d", &vol_mV);
@@ -74,28 +81,28 @@ float get_gpu_temp(void)
     return temp_value;
 }
 
-void set_cpufreq(int freq_val)
+void set_cpufreq(int freq)
 {
-    fprintf(set_cfreq, "%d", freq_val);
-    fflush(freq_val);
+    fprintf(set_cfreq, "%d", freq);
+    fflush(freq);
 }
 
 float* get_raw_state()
 {
     float state_buff[NUM_STATES];
-    float cpu_temp = get_cpu_temp();
-    float gpu_temp = get_gpu_temp();
-    float cpu_power = get_cpu_power();
-    float cpu_vol = get_cpu_vol();
-    float IOC = get_IOC();
+    state_buff[0] = get_cpu_temp();
+    state_buff[1] = get_gpu_temp();
+    state_buff[2] = get_cpu_power();
+    state_buff[3] = get_cpu_volts();
+    state_buff[4] = get_IPC();
 
     return state_buff;
 }
 
-double* update_q_off_policy(int last_state, int last_actions, int state, double reward) 
+double* update_q_off_policy(float* last_state, double last_actions, float* state, float reward) 
 {
-    double best_next_return = 0; 
-    double total_return;
+    float best_next_return = 0; 
+    float total_return;
     for (int i = 0; i < NUM_STATES; i++) {
         best_next_return = max(best_next_return, Q[i][0]);
     }
@@ -108,7 +115,7 @@ double* update_q_off_policy(int last_state, int last_actions, int state, double 
             old_value[i][j] = Q[i][j];   
         }
     }
-    // update q-table
+    // update q-table, by TD-method
     for (int i = 0; i < NUM_STATES; i++) {
         for (int j = 0; j < NUM_ACTIONS; j++) {
            Q[i][j] = old_value[i][j] + ALPHA * (total_return - old_value[i][j]);
@@ -116,45 +123,51 @@ double* update_q_off_policy(int last_state, int last_actions, int state, double 
     }
     return Q;
 }
-/*
-float reward_func()
+
+float reward_func(float* states, double new_freq)
 {
-    int thermal_penalty = 10;
-    //IPC
-    int reward += get_ipc();
-    if (get_temp() > TEMP_LIMIT) {
-        int reward -= thermal_penalty;        
+    float reward;
+    float cpu_volts, cpu_freq, cpu_temp, cpu_power, IPC, gpu_temp;
+    float temp_var, thermal_penalty, cur_power, max_power;
+    cpu_temp = states[0];
+    gpu_temp = states[1];
+    cpu_power = states[2];
+    cpu_volts = states[3];
+    IPC = states[4];
+
+    cur_power = new_freq * pow(cpu_volts, 2);  // dynamic power
+    max_power = cpufreq[NUM_FREQS - 1] * pow(MAX_CPU_VOLTS, 2);
+    temp_var = cpu_temp - TEMP_LIMIT;
+    
+    // reward judged by throughput because it can let system consider more performance aware
+    reward += (IPC / IPC_MAX);
+    if (temp_var > 0) { // overheat
+        thermal_penalty += (RHO * temp_var * (cur_power / max_power));
+        reward -= thermal_penalty;
     }
 
-    return IPC - power_penalty - thermal_penalty;
+    return reward;
 }
-*/
+
 void q_learning()
 {
     int episode = 0;
-    int last_state = 0;
-    int last_action = 0;
-    int reward = 0;
+    float reward = 0;
+    float last_states[NUM_STATES] = {0};
+    float* states = NULL;
+    int last_action_index = 0;
+    int best_action_index = 0;
     srand(time(NULL)); // set rand seed
 
     while (episode < MAX_EPISODE) {
-        int* state = get_raw_state();
+        states = get_raw_state();
         double best_next_return = 0; 
-
-        // Penalize trying to go out of bounds, since there is no utility in doing so.
-        if (action < MIN_CPUFREQ || action > MAX_CPUFREQ) {
-            if (action < MIN_CPUFREQ) {
-                reward = reward - (500 * (MIN_CPUFREQ - action);
-            }
-            else {
-                reward = reward - (500 * (action - MAX_CPUFREQ); 
-            }
-        }
+        
         
         // Update state-action-reward trace
-        if (last_state) {
-            reward = reward_func(state);
-            double* tmp = update_q_off_policy(last_state, last_action, state, reward);
+        if (last_states) {
+            reward = reward_func(states, cpufreq[best_action_index]);
+            double* tmp = update_q_off_policy(last_states, last_action_index, states, reward);
             for (int i = 0; i < NUM_STATES; i++) {
                 for (int j = 0; j < NUM_ACTIONS; j++) {
                     printf("%d ", tmp + i * NUM_STATES + NUM_ACTIONS);
@@ -163,18 +176,27 @@ void q_learning()
             }
         }
         else {
-            reward = reward_func(state);
+            reward = reward_func(states, cpufreq[best_action_index]);
         }
 
         if (rand() < episode) {
-            best_aciton = rand() % NUM_ACTIONS;
+            best_action_index = rand() % NUM_ACTIONS;
         }
         else {
             for (int i = 0; i < NUM_STATES; i++) {
-                best_next_return = max(best_next_return, Q[i][0]);
+                if (max(best_next_return, Q[i][0]) == Q[i][0])
+                    best_next_return = i;
             }
-            best_action = best_next_return;
+            best_action_index = best_next_return;
         }
+        // Penalize trying to go out of bounds, since there is no utility in doing so.
+        if (best_action_index < 0 || best_action_index > NUM_ACTIONS) {
+            if (best_action_index < 0) {
+                reward = reward - (500 * (NUM_ACTIONS - best_action_index));
+            }
+            reward = reward - (500 * (best_action_index - NUM_ACTIONS)); 
+        }
+
         /*
             _        _                     _   _             
            | |_ __ _| | _____    __ _  ___| |_(_) ___  _ __  
@@ -184,10 +206,11 @@ void q_learning()
                                                                      
         */
         // Take action
-        set_cpufreq(cpufreq[best_action]);    
+        set_cpufreq(cpufreq[best_action_index]);    
         
-        last_state = state;
-        last_action = best_action;
+        for (int i = 0; i < NUM_STATES; i++)
+            last_states[i] = states[i];
+        last_action_index = best_action_index;
         
         episode++;
     }
--- RL_DVFS/setup.sh
+++ RL_DVFS/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo userspace | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+echo 1907200 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
 
