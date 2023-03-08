814100214aae95dd3f09814dc26fed2b3522c149
tony <a0921444212@gmail.com>
2023-03-08 18:07:45 +0800

    split from setup.sh

---
+++ RL_DVFS/train.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo userspace | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+echo 1907200 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_setspeed
+sudo ./qlearning_gov train
+
