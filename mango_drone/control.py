7edcc6f57130e0752d922808831c71144938256b
tony <a0921444212@gmail.com>
2023-03-18 18:25:50 +0800

    update compare area size bug

--- mango_drone/control.py
+++ mango_drone/control.py
@@ -1,1 +1,3 @@
 from dronkit import connect
+
+vehicle = connect('127.0.0.1:14550', wait_ready=True)
--- mango_drone/image.py
+++ mango_drone/image.py
@@ -2,11 +2,28 @@ import jetson.inference
 import jetson.utils
 
 
+
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("/dev/video0")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
 while display.IsStreaming():
 	img = camera.Capture()
+	
 	detections = net.Detect(img)
+	
+	max_bb_center = 0
+	max_area_det = 0
+	for detection in detections:
+		print('@@@@@@@@@@@@@@@@@@')
+		print(detection.ClassID)
+		print(detection.Area)
+		print('!!!!!!!!!!!!!!!!!!')
+		if detection.Area > max_area_det:
+			max_area_det = detection.Area
+			max_bb_center = detection.Center
+	
+	jetson.utils.cudaDrawCircle(img, (max_bb_center), 10, (255, 0, 0))
+	jetson.utils.cudaDrawCircle(img, (img.width/2, img.height/2), 10, (255, 0, 0))
+	jetson.utils.cudaDrawCircle(img, (0, 0), 10, (0, 255, 0))
 	display.Render(img)
