d7b9e3b1e2cf111f11966e01f621c9ef1bd59b5a
tony <a0921444212@gmail.com>
2023-03-26 03:24:36 +0800

    add donchian_channel strategy

--- trading/turtle/way_of_turtle.py
+++ trading/turtle/way_of_turtle.py
@@ -13,7 +13,7 @@ class trading_binance:
         prices = self.client.get_ticker(symbol=investment_target)
         return prices
     def get_history(self, investment_target):
-        historical = self.client.get_historical_klines(investment_target, Client.KLINE_INTERVAL_1DAY, "8 Jun 2011")
+        historical = self.client.get_historical_klines(investment_target, Client.KLINE_INTERVAL_1DAY, "8 Jun 2022")
         hist_df = pd.DataFrame(historical)
         hist_df.columns = [ # TB means taker buy
                             'Open time', 'Open', 'High', 'Low', 
@@ -34,7 +34,28 @@ class trading_binance:
         #print(hist_df.info())
         return hist_df
           
+######## strategy #########
 
+def strategy＿donchian＿channel(data):
+    x1=data.Close>data.upper_band
+    print(f"data.Close= {data.Close}")
+    print(f"data.upper_band= {data.upper_band}")
+    x2=data.Close.shift(1)<data.upper_band.shift(1)
+    x=x1&x2
+    y1=data.Close<data.lower_band
+    y2=data.Close.shift(1)>data.lower_band.shift(1)
+    y=y1&y2
+    data.loc[x,'signal']='buy'
+    data.loc[y,'signal']='sell'
+    buy_date=pd.to_datetime(data.loc[data.signal == 'buy', 'Close time'], unit='s').dt.strftime('%Y-%m-%d')
+    sell_date=pd.to_datetime(data.loc[data.signal == 'sell', 'Close time'], unit='s').dt.strftime('%Y-%m-%d')
+    data.loc[x,'bc']=data[data.signal=='buy'].Close.round(2)
+    data.loc[y,'sc']=data[data.signal=='sell'].Close.round(2)
+    buy_close=data[data.signal=='buy'].Close.round(2).tolist()
+    sell_close=data[data.signal=='sell'].Close.round(2).tolist()
+
+    return (buy_date,buy_close,sell_date,sell_close)
+    
     
 ########### main ############
 
@@ -42,14 +63,34 @@ if __name__ == '__main__':
     api_key = input('Insert your api_key')
     api_secret = input('Insert your api_secret')
     
-    investment_target = 'BTCUSDT'
-    
+    investment_target = 'BTCUSDT'    
     trading = trading_binance(api_key, api_secret)
+    short_term = True
     
     prices = trading.get_daily_data(investment_target)
 
     history_prices = trading.get_history(investment_target)
     history_prices_describe = history_prices.describe()
+    
+    if (short_term):
+        channel_len = 20
+    else :
+        channel_len = 55
+    
+    history_prices['upper_band'] = history_prices['High'].shift(1).rolling(channel_len).max()
+    history_prices['lower_band'] = history_prices['Low'].shift(1).rolling(channel_len).min()
+   
+  
+    bd,bc,sd,sc = strategy＿donchian＿channel(history_prices)
+    
+
+
+    add_plots = [
+                mpf.make_addplot(history_prices['upper_band'], color='green'),
+                mpf.make_addplot(history_prices['lower_band'], color='red'),
+                mpf.make_addplot(history_prices['bc'].values, type='scatter', markersize=60, marker='^'),
+                mpf.make_addplot(history_prices['sc'].values, type='scatter', markersize=60, marker='v')
+                ]
     
     mpf.plot(
              history_prices.set_index('Close time'), 
@@ -57,9 +98,10 @@ if __name__ == '__main__':
              style='charles', 
              volume=True,
              title='BTCUSDT',
-             mav=(20,55)
+             mav=(20,55),
+             addplot=add_plots
              )
-    
+
     '''
     order = trading.create_test_order(
                                     symbol=investment_target,
