3497a59b6dd6456f3718073938d833d6643f9fa7
tony <a0921444212@gmail.com>
2023-03-25 20:41:47 +0800

    refresh get_history

--- trading/turtle/way_of_turtle.py
+++ trading/turtle/way_of_turtle.py
@@ -1,11 +1,6 @@
 import pandas as pd
 import numpy as np
-#import talib as ta
-#from datatime import datatime, timedelta
-#import matplot.pyplot as plt
-# show chinese and minus
-#%matplotlib inline
-from pylab import mpl
+import mplfinance as mpf
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 
 ######### function ########
@@ -18,15 +13,32 @@ class trading_binance:
         prices = self.client.get_ticker(symbol=investment_target)
         return prices
     def get_history(self, investment_target):
-        history_prices = []
-        for kline in self.client.get_historical_klines_generator(investment_target, Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC"):
-            history_prices.append(kline)
-        return history_prices
+        historical = self.client.get_historical_klines(investment_target, Client.KLINE_INTERVAL_1DAY, "8 Jun 2011")
+        hist_df = pd.DataFrame(historical)
+        hist_df.columns = [ # TB means taker buy
+                            'Open time', 'Open', 'High', 'Low', 
+                            'Close', 'Volume', 'Close time', 'Quote Asset Volume',
+                            'Number of trades', 'TB base asset volume',         
+                            'TB quote asset volume', 'Ignore'
+                          ]
+        hist_df['Open time'] = pd.to_datetime(hist_df['Open time']/1000, unit='s')
+        hist_df['Close time'] = pd.to_datetime(hist_df['Close time']/1000, unit='s')
+        
+        numeric_col = [
+                        'Open', 'High', 'Low', 'Close',
+                        'Volume', 'Quote Asset Volume',
+                        'TB base asset volume', 
+                        'TB quote asset volume'
+                      ]
+        hist_df[numeric_col] = hist_df[numeric_col].apply(pd.to_numeric, axis=1)
+        #print(hist_df.info())
+        return hist_df
+          
+
     
 ########### main ############
 
 if __name__ == '__main__':
-
     api_key = input('Insert your api_key')
     api_secret = input('Insert your api_secret')
     
@@ -37,7 +49,16 @@ if __name__ == '__main__':
     prices = trading.get_daily_data(investment_target)
 
     history_prices = trading.get_history(investment_target)
+    history_prices_describe = history_prices.describe()
     
+    mpf.plot(
+             history_prices.set_index('Close time'), 
+             type='candle', 
+             style='charles', 
+             volume=True,
+             title='BTCUSDT',
+             mav=(20,55)
+             )
     
     '''
     order = trading.create_test_order(
