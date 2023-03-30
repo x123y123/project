f4278572da85ea0a99c7c4f6f763c4437bd16529
tony <a0921444212@gmail.com>
2023-03-30 17:17:20 +0800

    update correct direction for movement

--- mango_drone/control.py
+++ mango_drone/control.py
@@ -5,13 +5,11 @@ import sys
 
 from dronekit import connect, VehicleMode,LocationGlobal,LocationGlobalRelative
 from pymavlink import mavutil
-#############################
 
-targetAltitude=1
-manualArm=False
 ############DRONEKIT#################
-
-
+#x for(+)_back(-)
+#y let(-)_right(+)
+#z up(-)_down(+)
 #########FUNCTIONS###########
 
 class drone_controller:
@@ -45,24 +43,8 @@ class drone_controller:
             time.sleep(1)
         
         self.vehicle.close()
-'''	   
-    def send_body_ned_velocity(self, velocity_x, velocity_y, velocity_z, duration):
-        msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
-                0,       # time_boot_ms (not used)
-                0, 0,    # target system, target component
-                #mavutil.mavlink.MAV_FRAME_BODY_NED, # frame Needs to be MAV_FRAME_BODY_NED for forward/back left/right control.
-                mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, # frame Needs to be MAV_FRAME_BODY_NED for forward/back left/right control.
-                0b0000111111000111, # type_mask
-                0, 0, 0, # x, y, z positions (not used)
-                velocity_x, velocity_y, velocity_z, # m/s
-                0, 0, 0, # x, y, z acceleration
-                0, 0)
-        for x in range(0,duration):
-            self.vehicle.send_mavlink(msg)
-            time.sleep(1)
-        self.vehicle.flush()
-'''   
-    def move_forward(self):
+    
+    def move_right(self):
         velocity_x = 0
         velocity_y = 0.25
         velocity_z = 0
@@ -83,7 +65,7 @@ class drone_controller:
             time.sleep(1)
         self.vehicle.flush()
         
-    def move_backward(self):
+    def move_left(self):
         velocity_x = 0
         velocity_y = -0.25
         velocity_z = 0
@@ -104,7 +86,8 @@ class drone_controller:
             time.sleep(1)
         self.vehicle.flush()
 
-    def move_left(self):
+
+    def move_backward(self):
         velocity_x = -0.25
         velocity_y = 0
         velocity_z = 0
@@ -125,7 +108,7 @@ class drone_controller:
             time.sleep(1)
         self.vehicle.flush()
 
-    def move_right(self):
+    def move_forward(self):
         velocity_x = 0.25
         velocity_y = 0
         velocity_z = 0
@@ -173,8 +156,7 @@ class drone_controller:
         velocity_y = 0
         velocity_z = 0.25
         duration = 2
-        
-	msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
+        msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
                 0,       # time_boot_ms (not used)
                 0, 0,    # target system, target component
                 #mavutil.mavlink.MAV_FRAME_BODY_NED, # frame Needs to be MAV_FRAME_BODY_NED for forward/back left/right control.
@@ -201,7 +183,7 @@ if __name__ == '__main__':
     
     control.takeoff(2)
     print("takeoff act!!")
-    '''
+    
     control.move_forward()
     print("forward act!!")
     time.sleep(1)
@@ -209,11 +191,11 @@ if __name__ == '__main__':
     control.move_left()
     print("left act!!")
     time.sleep(1)
-    '''
+    
     control.move_right()
     print("right act!!")
     time.sleep(1)
-    '''
+    
     control.move_backward()
     print("back act!!")
     time.sleep(1)
@@ -224,6 +206,6 @@ if __name__ == '__main__':
     control.move_down()
     print("down act!!")
     time.sleep(1)
-    '''
+    
     control.land()
     print("land act!!")
