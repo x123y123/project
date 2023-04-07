1728f05df656371374f106d3d1ac7996b7599f4f
tony <a0921444212@gmail.com>
2023-04-07 16:14:17 +0800

    split performance_model

---
+++ mango_drone/performance_model.py
@@ -0,0 +1,14 @@
+from typing import Any, Callable, List, TypeVar
+
+F = TypeVar("F", bound=Callable[..., Any])
+
+def performance_monitor(func: F) -> F:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"{func.__name__} took {execution_time} seconds to execute.") 
+        return result
+    wrapper.execution_times: List[float] = []
+    return wrapper
